a0e17b895cb91aa2eb829ee4d5e0e1d8b2f8e500
diegojimenez77 <djpcoding8652@gmail.com>
2023-06-29 15:56:25 -0600

    first release, pending category filters

--- react-course/src/Context/index.jsx
+++ react-course/src/Context/index.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 
 export const ShoppingCartContext = createContext()
 
@@ -22,6 +22,68 @@ export const ShoppingCartProvider = ({children}) => {
         //Shopping Cart - Add products to cart
     const [cartProducts, setCartProducts] = useState([])
 
+     //Shopping Cart - Order
+    const [order, setOrder] = useState([])
+
+    //Get produtcts
+    const [items, setItems] = useState(null)
+    
+        useEffect(() => {
+            fetch('https://fakestoreapi.com/products')
+            .then(response => response.json())
+            .then(data => setItems(data))
+        },[])
+
+    //Get proucts by title
+    const [searchByTitle, setSearchByTitle] = useState(null)
+  
+    const [filteredItems, setFilteredItems] = useState(null)
+
+    const filterdItemsByTitle = (items, searchByTitle) => {
+        return items?.filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
+    }
+
+    //      //Get proucts by Category
+    //  const [searchByCategory, setSearchByCategory] = useState(null)
+    //  console.log(searchByCategory)
+  
+    //  const [filteredItemsByCategory, setFilteredItemsByCategory] = useState(null)
+ 
+    //  const filterdItemsByCategory = (items, searchByCategory) => {
+    //     console.log('items: ', items)
+    //      return items?.filter(item => item.category.toLowerCase().includes(searchByCategory.toLowerCase()))
+    //  }
+     
+    //  const filterBy = (searchType, items, searchByTitle, searchByCategory) => {
+    //     if (searchType === 'BY_TITLE'){
+    //        return filterdItemsByTitle(items, searchByTitle)
+    //     }
+
+    //     if (searchType === 'BY_CATEGORY'){
+    //         return filterdItemsByCategory(items, searchByCategory)
+    //      }
+
+    //      if (searchType === 'BY_TITLE_AND_CATEGORY'){
+    //         return filterdItemsByCategory(items, searchByCategory).filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
+    //      }
+
+    //     if (!searchType) {
+    //         return items
+    //      }
+    //  }
+
+    //  useEffect(() => {
+    //     if (searchByTitle && searchByCategory) setFilteredItems(filterBy('BY_TITLE_AND_CATEGORY', items, searchByTitle, searchByCategory))
+    //     if (searchByTitle && !searchByCategory) setFilteredItems(filterBy('BY_TITLE', items, searchByTitle, searchByCategory))
+    //     if (searchByCategory && !searchByTitle) setFilteredItemsByCategory(filterBy('BY_CATEGORY', items, searchByCategory, searchByTitle))
+    //     if (!searchByCategory && !searchByTitle) setFilteredItemsByCategory(filterBy(null, items, searchByCategory, searchByTitle))
+    // }, [items, searchByTitle, searchByCategory])
+
+    useEffect(() => {
+        if (searchByTitle) setFilteredItems(filterdItemsByTitle(items, searchByTitle))
+    }, [items, searchByTitle])
+
+    
     return(
         <ShoppingCartContext.Provider value={{
             count,
@@ -35,7 +97,17 @@ export const ShoppingCartProvider = ({children}) => {
             setCartProducts,
             isCheckoutSideMenuOpen,
             openCheckoutSideMenu,
-            closeCheckoutSideMenu
+            closeCheckoutSideMenu,
+            order,
+            setOrder,
+            items,
+            setItems,
+            searchByTitle,
+            setSearchByTitle,
+            filteredItems,
+            // searchByCategory,
+            // setSearchByCategory,
+            // filteredItemsByCategory
         }}>
             {children}
         </ShoppingCartContext.Provider>
--- react-course/src/Pages/App/Index.jsx
+++ react-course/src/Pages/App/Index.jsx
@@ -14,9 +14,16 @@ import './App.css'
 const AppRoutes = () => {
   let routes = useRoutes([
     { path: '/',element: <Home /> },
+    { path: '/clothes',element: <Home /> },
+    { path: '/electronics',element: <Home /> },
+    { path: '/forniture',element: <Home /> },
+    { path: '/toys',element: <Home /> },
+    { path: '/other',element: <Home /> },
     { path: '/my-account',element: <MyAccount /> },
     { path: '/my-order',element: <MyOrder /> },
     { path: '/my-orders',element: <MyOrders /> },
+    { path: '/my-orders/last',element: <MyOrder /> },
+    { path: '/my-orders/:id',element: <MyOrder /> },
     { path: '/sign-in',element: <SignIn /> },
     { path: '/*',element: <NotFound /> }
 
